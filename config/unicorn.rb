6730bb98c4f81fad64bcd559704e880407e59487
Tommy Dang <quantumventuress@gmail.com>
2015-03-15 15:28:47 -0700

    unicorn listen to port 3

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,7 @@ timeout 30 # Restart any workers that haven't responded in 30 seconds
 
 # Listen on a Unix data socket
 # listen "#{shared_dir}/sockets/unicorn.sock", backlog: 64
-listen "127.0.0.1:3000"
+listen "127.0.0.1:80"
 
 stderr_path "#{shared_dir}/log/unicorn.log"
 stdout_path "#{shared_dir}/log/unicorn.log"
